ab77ece09249411c5fce7adbe2d361ebc42f4248
Abhishek <abhi.dwivedi6@gmail.com>
2022-07-25 19:41:12 +0530

    removed spaces

--- src/main/java/test/libraries/Utilities.java
+++ src/main/java/test/libraries/Utilities.java
@@ -5,67 +5,43 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-
 public class Utilities {
 
-		private void syncEvents(WebDriver driver, WebElement element, String string) {
-			switch(string) {
-				case "To Be Clickable":
-					new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(element));
-					break;
-					
+	private void syncEvents(WebDriver driver, WebElement element, String string) {
+		switch(string) {
+		case "To Be Clickable":
+			new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(element));
+			break;
 
 		case "To Be Visible":
 			new WebDriverWait(driver, 30).until(ExpectedConditions.visibilityOf(element));
 			break;
-			
+
 		case "To Alert":
 			new WebDriverWait(driver, 30).until(ExpectedConditions.alertIsPresent());
 			break;
 
-									
-				case "Alert Is Present":
-					new WebDriverWait(driver, 30).until(ExpectedConditions.alertIsPresent());
-					break;
-			}
-
+		case "Alert Is Present":
+			new WebDriverWait(driver, 30).until(ExpectedConditions.alertIsPresent());
+			break;
 		}
-		
-		public void HandleClickEvent(WebDriver driver, WebElement element) {
-			syncEvents(driver, element, "To Be Clickable");
-			element.click();
-		}
-		
 
-		
-	
+	}
+
+	public void HandleClickEvent(WebDriver driver, WebElement element) {
+		syncEvents(driver, element, "To Be Clickable");
+		element.click();
+	}
+
 	public void HandleVisibilityOfElements(WebDriver driver, WebElement element) {
 		syncEvents(driver, element, "To Be Visible");
 	}
 
-
-
-	public void HandleAlertEvent(WebDriver driver) {
-		syncEvents(driver, null, "To Alert");
+	public String HandleAlert(WebDriver driver) {
+		syncEvents(driver, null, "Alert Is Present");
+		String msg = driver.switchTo().alert().getText();
 		driver.switchTo().alert().accept();
-		
+		return msg;
 	}
-
-
-
-	public String Alerttext(WebDriver driver) {
-		
-		String text=driver.switchTo().alert().getText();
-		return text;
-	}
-
-		
-		public String HandleAlert(WebDriver driver) {
-			syncEvents(driver, null, "Alert Is Present");
-			String msg = driver.switchTo().alert().getText();
-			driver.switchTo().alert().accept();
-			return msg;
-		}
 
 }
